5198f9654dea3a87ddee8c27c2f04cdb393b069c
ljh95 <ilegwmam@gmail.com>
2020-03-26 15:37:45 +0900

    잡다한 테스트 커밋

---
+++ app.js
@@ -0,0 +1,87 @@
+var express = require("express");
+var login = require('./routes/loginroutes');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+app.use( bodyParser.urlencoded({ extended: true }) );
+app.use( bodyParser.json() );
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+var router = express.Router();
+
+
+// test route
+router.get('/', function(req, res) {
+    res.json({ message: 'welcome to our upload module apis' });
+});
+
+
+// route to handle user registration
+router.post('/register', login.register);
+router.post('/login', login.login)
+app.use('/api', router);
+app.listen(5000);
+
+exports.register = function (req, res) {
+    // console.log("req", req.body);
+    var today = new Date();
+    var users = {
+        "first_name": req.body.first_name,
+        "last_name": req.body.last_name,
+        "email": req.body.email,
+        "password": req.body.password,
+        "created": today,
+        "modified": today
+    }
+    connenction.query('INSERT INTO users SET ?' , users, function (error, results, fields) {
+        if (error) {
+            console.log("error ocurred", error);
+            res.send({
+                "code" : 400,
+                "failed": "error ocurred"
+            })
+        } else {
+            console.log('The solution is: ', results);
+            res.send({
+                "code": 200,
+                "success": "user registered sucessfully"
+            });
+        }
+    });    
+}
+
+exports.register = function (req, res) {
+    // console.log("req", req.body);
+    var today = new Date();
+    var users = {
+        "first_name": req.body.first_name,
+        "last_name": req.body.last_name,
+        "email": req.body.email,
+        "password": req.body.password,
+        "created": today,
+        "modified": today
+    }
+    connenction.query('INSERT INTO users SET ?' , users, function (error, results, fields) {
+        if (error) {
+            console.log("error ocurred", error);
+            res.send({
+                "code" : 400,
+                "failed": "error ocurred"
+            })
+        } else {
+            console.log('The solution is: ', results);
+            res.send({
+                "code": 200,
+                "success": "user registered sucessfully"
+            });
+        }
+    });    
+}
