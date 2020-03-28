44657cf434003a8ad6b9e10532bafe374ff999d6
ljh95 <ilegwmam@gmail.com>
2020-03-28 10:58:06 +0900

    /board에서 post형태의 요청이 들어오면 받아주는 router생성

--- BackEnd/0328_node_boardPJ/seq-crude-exam/routes/index.js
+++ BackEnd/0328_node_boardPJ/seq-crude-exam/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+const models = require('../models');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/board', function(req, res, next) {
+  res.render('show');
+});
+
+router.post('/board', function(req, res, next){
+  let body = req.body;
+
+  models.post.create({
+    title: body.inputTitle,
+    writer: body.inputWriter
+  })
+  .then( result => {
+    console.log("데이터 추가 완료");
+    res.redirect("/board");
+  })
+  .catch( err=> {
+    console.log("데이터 추가 실패");
+  })
 });
 
 module.exports = router;
