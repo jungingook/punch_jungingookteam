c469a76491db178931d6cf95dc6b3f670fa3e46c
YeoEunseong <e7838752@gmail.com>
2020-04-10 17:26:02 +0900

    한글로 qr 생성되는지 테스트

--- Windows/qrcode/makeQR.py
+++ Windows/qrcode/makeQR.py
@@ -1,7 +1,7 @@
 import qrcode
 
 # Hello World! 로 QR 코드 생성
-img = qrcode.make('Hello World!')
+img = qrcode.make('한글로 qr코드 만들어지는 테스트')
 
 # 생성된 이미지를 helloworld_qrcode.png로 저장
 img.save('qrcode.png')
