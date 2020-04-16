8803de3377fceb7958412a17372c8da16c7fc4ff
YeoEunseong <e7838752@gmail.com>
2020-04-16 23:32:50 +0900

    http 통신 관련 커밋

---
+++ Windows/http/httpMakeQR.py
@@ -0,0 +1,52 @@
+import sys
+import qrcode
+import requests
+
+
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from PIL import ImageQt
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        #서버에서 json 값을 받아와 data 변수에 저장
+        url = "http://ec2-54-180-94-182.ap-northeast-2.compute.amazonaws.com:3000/desk/qr"
+        data = requests.get(url).json()
+
+        # data 딕셔너리 중 randomNum 키의 value 값으로 qr코드 생성, 추후 다른 키 값과 조합하여 수정 예정
+        img = qrcode.make(data['randomNum'])
+
+
+        qt_image = ImageQt.ImageQt(img)
+        pixmap = QPixmap.fromImage(qt_image)
+
+
+        lbl_img = QLabel()
+        lbl_img.setPixmap(pixmap)
+        lbl_size = QLabel('Width: '+str(pixmap.width())+', Height: '+str(pixmap.height()))
+        lbl_size.setAlignment(Qt.AlignCenter)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lbl_img)
+        vbox.addWidget(lbl_size)
+        self.setLayout(vbox)
+
+        self.setWindowTitle('QPixmap')
+        self.move(300, 300)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
+
+
+
