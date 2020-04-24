0dd6711cb47f4dc29a7f5acc425043ad64c61b65
Yeo, Eunseong <e7838752@gmail.com>
2020-04-24 16:53:53 +0900

    #20 실시간으로 label을 바꾸면서 프로그램 제어도 가능한 소스코드

--- Windows/test/QLabelTest.py
+++ Windows/test/QLabelTest.py
@@ -1,4 +1,4 @@
-import sys
+import sys, time, threading
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 
 
@@ -10,14 +10,26 @@ class MyApp(QWidget):
 
     def initUI(self):
 
-        label = QLabel('First Label', self)
-        label.setText("Test")
+        self.label = QLabel('First Label', self)
 
         layout = QVBoxLayout()
-        layout.addWidget(label)
+        layout.addWidget(self.label)
 
         self.show()
         self.setLayout(layout)
+
+        t = threading.Thread(target=self.changeText)
+        t.start()
+
+    def changeText(self):
+        count = 0
+
+        while True:
+            text = "Test" + str(count)
+            self.label.setText(text)
+            time.sleep(1)
+            count += 1
+            print(text)
 
 
 if __name__ == '__main__':
