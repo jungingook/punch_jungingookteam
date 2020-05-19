c7a754a7a7f4ae73300bc084489e1105215a689c
Yeo, EunSeong <e7838752@gmail.com>
2020-05-20 00:12:12 +0900

    #30 로그인 폼 commit

--- Windows/login/loginEx2.py
+++ Windows/login/loginEx2.py
@@ -1,12 +1,15 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout, QMessageBox, QHBoxLayout
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 
 
 class LoginForm(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Login Form')
+        self.setStyleSheet("background-color:white;")
+
+        self.setWindowTitle('펀치 QR 생성')
         self.resize(300, 500)
 
         layout = QGridLayout()
@@ -14,28 +17,34 @@ class LoginForm(QWidget):
         self.lbl = QLabel()
         self.img = QPixmap("logo.png")
         self.lbl.setPixmap(self.img.scaled(300, 300))
+        self.lbl.setStyleSheet("background-color:white;")
+        self.lbl.setAlignment(Qt.AlignCenter)
 
         layout.addWidget(self.lbl, 0, 0)
 
         self.lineEdit_username = QLineEdit()
+        self.lineEdit_username.setStyleSheet("font-size: 20px;")
         self.lineEdit_username.setPlaceholderText('아이디')
+        self.lineEdit_username.resize(50, 10)
+
         layout.addWidget(self.lineEdit_username, 1, 0)
 
         self.lineEdit_password = QLineEdit()
+        self.lineEdit_password.setStyleSheet("font-size: 20px;")
+        self.lineEdit_password.setEchoMode(QLineEdit.Password)
         self.lineEdit_password.setPlaceholderText('비밀번호')
         layout.addWidget(self.lineEdit_password, 2, 0)
 
         button_login = QPushButton('Login')
         button_login.clicked.connect(self.check_password)
         layout.addWidget(button_login, 3, 0)
-        layout.setRowMinimumHeight(2, 75)
 
         self.setLayout(layout)
 
     def check_password(self):
         msg = QMessageBox()
 
-        if self.lineEdit_username.text() == 'Username' and self.lineEdit_password.text() == '000':
+        if self.lineEdit_username.text() == 'ingook' and self.lineEdit_password.text() == 'babo':
             msg.setText('Success')
             msg.exec_()
             app.quit()
